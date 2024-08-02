c0f75efa987f2740c6eb173076ff3c8f0fed613e
Kevin Sylvestre <kevin@ksylvest.com>
2024-08-01 19:46:30 -0700

    Call to-sql to de-dupe scopes

--- lib/graphql/sources/active_record_base.rb
+++ lib/graphql/sources/active_record_base.rb
@@ -6,7 +6,16 @@ module GraphQL
     class ActiveRecordBase < GraphQL::Dataloader::Source
       # @param model [Class] a child class of ActiveRecord::Base (e.g. Comment)
       # @param key [Symbol] an attribute (typically a foreign key) to use for loading (e.g. :user_id)
-      def initialize(model, key:)
+      # @return [Array] a key
+      def self.batch_key_for(model, key: :id)
+        return [model.to_sql, key] if model.is_a?(ActiveRecord::Relation)
+
+        [model, key]
+      end
+
+      # @param model [Class] a child class of ActiveRecord::Base (e.g. Comment)
+      # @param key [Symbol] an attribute (typically a foreign key) to use for loading (e.g. :user_id)
+      def initialize(model, key: :id)
         super()
         @model = model
         @key = key
